91dda2901fdd60a56a18f16586d51cf6f9a4398a
vladaunski <112953613+vladaunski@users.noreply.github.com>
2025-06-16 17:00:26 +0200

    Finalize DORA plugin integration
    Finished configuration after migration of plugin.

--- workspaces/dora-explorer/packages/app/src/components/Root/Root.tsx
+++ workspaces/dora-explorer/packages/app/src/components/Root/Root.tsx
@@ -75,6 +75,8 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={ExtensionIcon} to="dora-dashboard" text="DORA Plugin" />
+      
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
